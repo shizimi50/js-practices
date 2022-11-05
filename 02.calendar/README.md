d353d8cb3282b517c7c7ca42b4f29dd80e3a9c25
shizimi50 <mjrwrbbc@gmail.com>
2022-11-05 15:17:50 +0900

    リファクタリング - 不要なコメントを削除 - テンプレートリテラルを活用 - 冗長なコードを修正
    READMEに実行コマンドを追加

---
+++ 02.calendar/README.md
@@ -0,0 +1,15 @@
+# js-practices
+
+フィヨルドブートキャンプのJSプラクティスの提出物をまとめるリポジトリです。
+
+# How to use
+
+使い方は基本的に[ruby-practices](https://github.com/fjordllc/ruby-practices/)と同じです。こちらのREADMEを参照してください。rubocopはeslintに読み替えてください。
+
+## 実行コマンド
+```
+$ cd 02.calendar
+
+$ node cal.js -y 2022 -m 11
+
+```
--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,56 +1,33 @@
-const args = require('minimist')(process.argv.slice(2)) // { _: [], y: 2022, m: 9 }
-const year = args.y // コマンド引数で年を取得
-const month = args.m // コマンド引数で月を取得
-const weekdays = ['日', '月', '火', '水', '木', '金', '土'] // 曜日を初期化
-const startDate = new Date(year, month - 1) // 月初の日付を取得
-const startWday = startDate.getDay() // 月初の曜日を取得
-const lastDate = new Date(new Date(year, month, 0)) // 月末の日付を取得
-const oneSpace = ' ' // 半角空白を初期化
-let firstSaturday // 最初の土曜日の日付を宣言
+const args = require('minimist')(process.argv.slice(2))
+const year = args.y
+const month = args.m
+const weekdays = ['日', '月', '火', '水', '木', '金', '土']
+const startDate = new Date(year, month - 1)
+const startWday = startDate.getDay()
+const lastDate = new Date(new Date(year, month, 0))
+const oneSpace = ' '
 
-// ========コマンド引数で取得した年月を出力========
-console.log((oneSpace * 5) + year + '年' + 'month')
-// ========全曜日を出力========
+// NOTE: 空白を5行いれることで、年月日をセンタリング
+console.log(`${'     '}${year}年${month}月`)
+
 for (let i = 0; i < weekdays.length; ++i) {
   process.stdin.write(`${weekdays[i]} `)
 }
-// ========曜日と日付を改行するためにconsole.logで改行を追加========
+// NOTE: 曜日と日付を改行するために、console.logで空文字を出力
 console.log('')
 
-// ========開始日の左側にスペースを出力する（条件は曜日）========
-if (startWday === 0) { // 月初が日曜日 空白0個
-  process.stdout.write(oneSpace.repeat(0))
-  firstSaturday = 7
-} else if (startWday === 1) { // 月初が月曜日 空白1個
-  firstSaturday = 6
-  process.stdout.write(oneSpace.repeat(3))
-} else if (startWday === 2) { // 月初が火曜日 空白2個
-  process.stdout.write(oneSpace.repeat(6))
-  firstSaturday = 5
-} else if (startWday === 3) { // 月初が水曜日 空白3個
-  process.stdout.write(oneSpace.repeat(9))
-  firstSaturday = 4
-} else if (startWday === 4) { // 月初が木曜日 空白4個
-  process.stdout.write(oneSpace.repeat(12))
-  firstSaturday = 3
-} else if (startWday === 5) { // 月初が金曜日 空白5個
-  process.stdout.write(oneSpace.repeat(15))
-  firstSaturday = 2
-} else { // 月初が土曜日 空白6個
-  process.stdout.write(oneSpace.repeat(18))
-  firstSaturday = 1 // 最初の土曜日
-}
+process.stdout.write(oneSpace.repeat(startWday * 3))
+const firstSaturday = 7 - startWday
 
-// ========日付が2桁と1桁で空白追加の有無を定義 + 土曜日の場合は改行して出力をさせる========
 for (let i = 1; i <= lastDate.getDate(); ++i) {
   if (String(i).length > 1) { // 二行の場合
-    if (i === firstSaturday || i === (firstSaturday + 7) || i === (firstSaturday + 14) || i === (firstSaturday + 21) || i === (firstSaturday + 28)) {
+    if (i === firstSaturday || (i - firstSaturday) % 7 === 0) {
       console.log(i + oneSpace)
     } else {
       process.stdout.write(i + oneSpace)
     }
   } else {
-    if (i === firstSaturday || i === (firstSaturday + 7) || i === (firstSaturday + 14) || i === (firstSaturday + 21) || i === (firstSaturday + 28)) {
+    if (i === firstSaturday || (i - firstSaturday) % 7 === 0) {
       console.log(oneSpace + i + oneSpace)
     } else {
       process.stdout.write(oneSpace + i + oneSpace) // 一行の場合
